d6a46e97f2e19825e01b2c1d3d2d04bda45ac922
Kamil Radzimowski <lukasiwa@poczta.onet.pl>
2022-06-24 15:31:36 +0200

    list item styling

--- my-app/src/MovieListItem.tsx
+++ my-app/src/MovieListItem.tsx
@@ -6,7 +6,11 @@ function MovieListItem(props) {
         <div className="item">
             <img alt={`${props.data.title} movie poster`} className="item-img" src={`https://image.tmdb.org/t/p/original/${props.data.poster}`}></img>
             <div className="item-data">
-                <div className="item-title">{`${props.data.title}`}</div>
+                <div className="item-title">
+                    <div className="title">{`${props.data.title}`}</div>
+                    <div className="popularity">{`Popularność: ${props.data.popularity}`}</div>
+                    <div className="vote-num">{`Liczba głosów ${props.data.vote_count}`}</div>
+                </div>
                 <div className="item-desc">{`${props.data.overview}`}</div>
             </div>
         </div>
