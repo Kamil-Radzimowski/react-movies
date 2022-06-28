babc21ca9dd4b0082e5c59d5d65055d8d4a83b16
Kamil Radzimowski <lukasiwa@poczta.onet.pl>
2022-06-28 21:57:15 +0200

    changed from divs to materialUI in moviePage

--- my-app/src/moviePage/MoviePage.tsx
+++ my-app/src/moviePage/MoviePage.tsx
@@ -4,7 +4,7 @@ import {useNavigate, useParams} from "react-router-dom";
 import '../assets/styleMoviePage.scss';
 import movie_logo from "../assets/the-movie-db-logo.svg";
 import { Gradient } from 'react-gradient';
-import {Rating} from "@mui/material";
+import {Box, Button, Card, CardActions, CardContent, CardMedia, Rating, Typography} from "@mui/material";
 import { useGetMovieDetailsByIdQuery } from "../Util/MovieService";
 
 
@@ -27,22 +27,36 @@ const MoviePage = () => {
                 {isLoading ? null : <div className="page-nav-title">{`${data?.title}`}</div>}
             </div>
             {isLoading ? null : <div className="page-content">
-                <img src={`https://image.tmdb.org/t/p/original/${data?.poster_path}`}></img>
-                <div className="text-content">
-                    <div className="top-content">
-                        {data?.genres.map((genre) => {return (<Gradient className='genre' key={genre.id} gradients={gradient} property='background' angle='0deg'>{`${genre.name}`}</Gradient>)})}
-                        <Rating className='rating' readOnly value={(data?.vote_average ?? 0) / 2} precision={0.25}></Rating>
-                    </div>
-                    <div className="desc">{`${data?.overview}`}</div>
-                    <div className='production-country'>{`Kraj Produkcji: ${data?.production_countries.reduce((acc, current, index) => {
-                        if(index == 0) {
-                            return acc.concat(current.name)
-                        }
-                        else{
-                           return acc.concat(', ').concat(current.name)
-                        }
-                    }, "")}`}</div>
-                </div>
+                <Card sx={{display: 'flex'}}>
+                    <CardMedia
+                        sx = {{width: 250}}
+                        component="img"
+                        image={`https://image.tmdb.org/t/p/original/${data?.poster_path}`}
+                    />
+                    <Box sx={{display: 'flex', flexDirection: 'column', flex: '1'}}>
+                        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                            <CardContent sx={{display: 'flex', alignItems: 'center', justifyContent: 'flex-start', flex: '1 0 auto', flexWrap: 'wrap'}}>
+                                {data?.genres.map((genre) => {return (<Typography sx={{paddingRight: 2}} key={genre.id}>{`${genre.name}`}</Typography>)})}
+                                <Rating sx={{paddingLeft: 2}} className='rating' readOnly value={(data?.vote_average ?? 0) / 2} precision={0.25}></Rating>
+                            </CardContent>
+                            <CardContent>
+                                <Typography>
+                                    {`${data?.overview}`}
+                                </Typography>
+                            </CardContent>
+                            <CardContent>
+                                <Typography>{`Kraj Produkcji: ${data?.production_countries.reduce((acc, current, index) => {
+                                    if(index == 0) {
+                                        return acc.concat(current.name)
+                                    }
+                                    else{
+                                        return acc.concat(', ').concat(current.name)
+                                    }
+                                }, "")}`}</Typography>
+                            </CardContent>
+                        </Box>
+                    </Box>
+                </Card>
             </div>}
         </div>
     )
