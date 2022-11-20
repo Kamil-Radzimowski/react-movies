0005f3fff47f82a65d81d553e4f5955865de54f5
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2022-11-20 16:17:33 +0100

    two gets added

--- my-app/src/backend/server.js
+++ my-app/src/backend/server.js
@@ -1,13 +1,34 @@
 import express from 'express'
+import data from "my-app/src/backend/backendAssets/data.json"
 const app = express()
 const port = 3000
 
 const database = {
-    users: []
+    users: [],
+    data: data
 }
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const simplifyMovie = (movie) => {
+    return {
+        id: movie.id, title: movie.title, popularity: movie.popularity, poster_path: movie.poster_path, vote_count: movie.vote_count, overview: movie.overview
+    }
+}
+
+app.get('/recommendation', (req, res) => {
+    const result = database.data.slice(0, 5).map((entry) => {return simplifyMovie(entry)})
+    res.send({results: result})
+})
+
+app.get('/search', (req, res) => {
+    const query = req.query.query
+    const page = req.query.page
+    const matchingMovies = database.data.reduce((acc, current) => {
+        if(current.title.includes(query)){
+            acc.push(simplifyMovie(current))
+        }
+        return acc;
+    }, [])
+    res.send({results: matchingMovies})
 })
 
 app.listen(port)
