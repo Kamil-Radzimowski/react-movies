13a1ef0c097a4c1e0f6360b3deb56abc4679523a
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2022-11-27 15:36:21 +0100

    dialog validation added

--- my-app/src/login/LoginDialog.tsx
+++ my-app/src/login/LoginDialog.tsx
@@ -7,47 +7,82 @@ type property = {
 }
 
 function LoginDialog(props: property){
-    const [nameError, setNameError] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const [emailError, setEmailError] = useState('')
     const [passwordError, setPasswordError] = useState('')
 
     function validateInput(){
-        // test
+        if(email.length === 0){
+            setEmailError("Email is required!")
+        }
+        else if(!validateEmail(email)){
+            setEmailError("Invalid Email!")
+        } else {
+            setEmailError("")
+        }
+
+        if(password.length === 0){
+            setPasswordError('Password is required!')
+        }
     }
 
-    return <Dialog open={props.open} onClose={props.onClose} fullWidth={true}
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
+    const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
+    const handleClose = () => {
+        props.onClose()
+        setPassword('')
+        setEmail('')
+        setPasswordError('')
+        setEmailError('')
+    }
+
+    return <Dialog open={props.open} onClose={handleClose} fullWidth={true}
                    maxWidth={'md'}>
         <DialogTitle>Zaloguj się</DialogTitle>
         <DialogContent>
             <Stack spacing={2}>
                 <TextField
                     autoFocus
-                    margin="dense"
-                    id="name"
-                    label="Nazwa użytkownika"
-                    helperText={nameError}
-                />
-                <TextField
-                    autoFocus
+                    error={emailError.length !== 0}
                     margin="dense"
                     id="email"
                     label="Adres email"
                     type="email"
+                    value={email}
+                    onChange={handleEmail}
                     helperText={emailError}
                 />
                 <TextField
                     autoFocus
+                    error={passwordError.length !== 0}
                     margin="dense"
                     id="password"
                     label="Hasło"
                     type="password"
+                    value={password}
+                    onChange={handlePassword}
                     helperText={passwordError}
                 />
             </Stack>
         </DialogContent>
         <DialogActions>
             <Button variant="outlined">Zarejestruj się</Button>
-            <Button variant="contained">Zaloguj się</Button>
+            <Button variant="contained" onClick={validateInput}>Zaloguj się</Button>
         </DialogActions>
     </Dialog>
 }
