7435b7c378f7daec638f2bf7c14d7df26254b764
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2022-12-13 20:55:54 +0100

    mqtt

--- my-app/src/admin/NotificationSender.tsx
+++ my-app/src/admin/NotificationSender.tsx
@@ -1,16 +1,28 @@
 import {Button, Card, CardActions, CardContent, CardHeader, TextField, Typography} from "@mui/material";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import mqtt from 'mqtt/dist/mqtt'
+
+const options = {
+    // Clean session
+    clean: true,
+    // Auth
+    clientId: 'NuWw7',
+}
 
 
 function NotificationSender() {
     const [text, setText] = useState('')
+
+    useEffect(() => {
+        const client  = mqtt.connect('mqtt://localhost:1883', options)
+    })
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setText(event.target.value)
     }
 
     const sendNotification = () => {
-
+        // client.publish("notification", text)
     }
 
     return <>
